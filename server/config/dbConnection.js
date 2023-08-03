1735f0a86974e1e32512f02d99176eb867c08366
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-08-03 22:08:22 +0530

    config added

---
+++ server/config/dbConnection.js
@@ -0,0 +1,16 @@
+import { connect } from "mongoose";
+import "dotenv/config";
+
+const MONGO_URI = process.env.MONGO_URI;
+
+const connectToDB = async () => {
+  await connect(MONGO_URI)
+    .then((conn) => {
+      console.log(`Connected to ${conn.connection.host}`);
+    })
+    .catch((e) => {
+      console.log(e.message);
+    });
+};
+
+export default connectToDB;
