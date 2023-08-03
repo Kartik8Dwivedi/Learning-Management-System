b5e96d5d787623384ff89f4da1c0c60a3769048c
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-08-03 22:08:45 +0530

    model added

---
+++ server/model/user.model.js
@@ -0,0 +1,68 @@
+import { Schema, model } from 'mongoose'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+const userSchema = new Schema({
+    fullName : {
+        type:String,
+        required : [true, "Name is required"],
+        minLength : [5, "Name must be greater than 5 characters"],
+        maxLength : [50, "Name must be greater than 50 characters"],
+        trim:true,
+    },
+    email: {
+        type:String,
+        required:[true, "Email is required"],
+        unique:[true, "This email is already registered, Sign in instead"],
+        lowercase : true,
+        trim : true,
+        match : [/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/, "Please provide a valid email address"],
+    },
+    password: {
+        type:String,
+        required : [true, "Password is required"],
+        minLength : [8, "Password must be atleast 8 characters"],
+        select : false,
+    },
+    role:{
+        type:String,
+        enum: ['USER', 'ADMIN'],
+        default: 'USER'
+    },
+    avatar:{
+        public_id:{
+            type:String,
+        },
+        secure_url : {
+            type:String,
+        }
+    },
+    forgotPasswordToken : String,
+    forgotPasswordExpiry : Date
+}, { timestamps : true })
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')){
+        return next()
+    }
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+userSchema.methods = {
+    comparePassword : async function(plainTextPassword) {
+        return await bcrypt.compare(plainTextPassword, this.password);
+    },
+    generateJWTToken : async function() {
+        return jwt.sign(
+            { id:this._id, role:this.role, email:this.email, subscrption:this.subscrption },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: process.env.JWT_EXPIRY
+            }
+        )
+    }
+}
+
+const User = model('User', userSchema)
+
+export default User
