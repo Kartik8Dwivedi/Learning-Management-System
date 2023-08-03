fa7f45674e73540a155c02405ed52fa8d0170ab9
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-08-03 22:10:06 +0530

    middlewares updated

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,20 @@
+import AppError from "../utils/appError.js";
+
+const isLoggedIn = function (req, res, next) {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return next(new AppError("Unauthenicated, please login", 401));
+  }
+  const tokenDetails = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (!tokenDetails) {
+    return next(new AppError("Unauthenicated, please login", 401));
+  }
+
+  req.user = tokenDetails;
+
+  next();
+};
+
+export default isLoggedIn
