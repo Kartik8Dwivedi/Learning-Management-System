ad691ccb302975d8b0116f0902ee26c38e8896b6
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-08-27 16:26:26 +0530

    reset password functionality added

---
+++ server/reset.readme.md
@@ -0,0 +1,3 @@
+This will be done in 2 sub parts/problem statements:
+- Enter Email > Validate in database > Generate Token > Send email with new url containing token + save token with expiry in database
+- Get token from url + new password > verify token in db > update password in database
---
+++ server/utils/sendEmail.js
@@ -0,0 +1,24 @@
+import nodemailer from "nodemailer";
+
+const sendEmail = async function (email,subject,message){
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+        user: process.env.SMTP_USERNAME,
+        pass: process.env.SMTP_PASSWORD
+  }
+});
+
+let info = await transporter.sendMail({
+    from:'"Kartik Dwivedi" <freelance.kartikdwivedi@gmail.com>',
+    to: email,
+    subject,
+    text:message,
+
+})
+console.log("Message sent: %s", info.messageId)
+
+}
