bb9963684393242461c4f312736feac3ad0498f2
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2023-12-19 01:27:48 +0530

    react-redux-toolkit setup done

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,20 @@
 // @ts-nocheck
+// @ts-ignore
 import "./index.css";
+import store from "./redux/store";
 
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-    <Toaster></Toaster>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
 );
